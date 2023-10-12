014c989620673beed4e41586dd4ee1c868d214fa
estheroche <you@example.com>
2023-10-12 14:24:02 -0500

    testt

--- test/Fractionized-NFT.t.sol
+++ test/Fractionized-NFT.t.sol
@@ -4,3 +4,34 @@ pragma solidity ^0.8.13;
 import {FractionalizedNFT} from "../src/Fractionized-NFT.sol";
 import "../src/ERC721Mock.sol";
 import "./Helpers.sol";
+
+contract FractionalizedNFTTest is Helpers {
+    FractionalizedNFT marketPlace;
+    Treasure nft;
+
+    function setup() public {
+        nft = new Treasure();
+    }
+
+    function testNotOwnerListing() public {
+        listing.listerAddress = addrB;
+        switchSigner(addrB);
+
+        vm.expectRevert(NFTMarket.NotOwner.selector);
+        marketPlace.createCustomListing(listing);
+    }
+
+    function testNonApproved() public {
+        switchSigner(addrA);
+        vm.expectRevert(NFTMarket.NotApproved.selector);
+        marketPlace.createCustomListing(listing);
+    }
+
+    function testMinPriceTooLow() public {
+        switchSigner(addrA);
+        nft.setApprovalForAll(address(marketPlace), true);
+        listing.priceInWei = 0;
+        vm.expectRevert(NFTMarket.MinPriceTooLow.selector);
+        marketPlace.createCustomListing(listing);
+    }
+}
